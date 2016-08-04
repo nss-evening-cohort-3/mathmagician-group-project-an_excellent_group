513845fbfacf983a7ea82000581534a6add86d20
Ryan B <admirallasers@gmail.com>
2016-08-03 19:55:00 -0500

    accepts numerical input
    the program now prompts the user for the number of numbers they want.

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -11,7 +11,10 @@ namespace Mathmagician
         static void Main(string[] args)
         {
             string requestInput = "What would you like me to do?";
+            string requestNumber = "How many should I print?";
             string prompt = "> ";
+            string makeIntoAnInt = "";
+            int printThisManyNumbers = 0; 
             Console.WriteLine("You have summoned the Mathmagician! You must be in need of numbers.");
             Console.WriteLine("VERY WELL! Input one of my magic words, press the Enter key and watch in awe.");
             Console.WriteLine("The magic words are: integer, fibonacci, prime, even and odd.");
@@ -20,27 +23,42 @@ namespace Mathmagician
             string userInput = Console.ReadLine().ToLower();
             if (userInput == "integer")
             {
-                Console.WriteLine("Integer method requested. press any key to close.");
+                Console.WriteLine("AHA! INTEGERS! " + requestNumber);
+                makeIntoAnInt = Console.ReadLine();
+                printThisManyNumbers = Int32.Parse(makeIntoAnInt);
+                Console.WriteLine("Printing " + printThisManyNumbers + " integers! MAGIC!");
                 Console.ReadLine();
             }
             else if (userInput == "fibonacci")
             {
-                Console.WriteLine("fibonacci method requested. press any key to close.");
+                Console.WriteLine("AHA! THE FIBONACCI SEQUENCE! " + requestNumber);
+                makeIntoAnInt = Console.ReadLine();
+                printThisManyNumbers = Int32.Parse(makeIntoAnInt);
+                Console.WriteLine("Printing " + printThisManyNumbers + " Fibonaccis! MAGIC!");
                 Console.ReadLine();
             }
             else if (userInput == "prime")
             {
-                Console.WriteLine("prime method requested. press any key to close.");
+                Console.WriteLine("AHA! PRIME NUMBERS! " + requestNumber);
+                makeIntoAnInt = Console.ReadLine();
+                printThisManyNumbers = Int32.Parse(makeIntoAnInt);
+                Console.WriteLine("Printing " + printThisManyNumbers + " primes! MAGIC!");
                 Console.ReadLine();
             }
             else if (userInput == "even")
             {
-                Console.WriteLine("even method requested. press any key to close.");
+                Console.WriteLine("AHA! EVENS! " + requestNumber);
+                makeIntoAnInt = Console.ReadLine();
+                printThisManyNumbers = Int32.Parse(makeIntoAnInt);
+                Console.WriteLine("Printing " + printThisManyNumbers + " evens! MAGIC!");
                 Console.ReadLine();
             }
             else if (userInput == "odd")
             {
-                Console.WriteLine("odd method requested. press any key to close.");
+                Console.WriteLine("AHA! ODDS! " + requestNumber);
+                makeIntoAnInt = Console.ReadLine();
+                printThisManyNumbers = Int32.Parse(makeIntoAnInt);
+                Console.WriteLine("Printing " + printThisManyNumbers + " odds! MAGIC!");
                 Console.ReadLine();
             }
             else
