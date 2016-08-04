63abc867c5138a980badae3d38bc2c37f6d35a10
Ryan B <admirallasers@gmail.com>
2016-08-04 17:14:50 -0500

    added unit tests

--- Mathmagician.Tests/IntegerTests.cs
+++ Mathmagician.Tests/IntegerTests.cs
@@ -15,7 +15,23 @@ namespace Mathmagician.Tests
             Integer my_int = new Integer();
             //assert
             Assert.IsNotNull(my_int);
-
+        }
+        [TestMethod]
+        public void GenerateAnIntTypeVariable()
+        {
+            Integer new_int = new Integer();
+            new_int.GenerateIntegers(1);
+            int intToTest = new_int.newInteger;
+            Assert.IsNotNull(intToTest);
+        }
+        [TestMethod]
+        public void IsTheNextNumberADifferentInteger()
+        {
+            Integer int_sequence = new Integer();
+            int_sequence.GenerateIntegers(5);
+            int last_int = int_sequence.integers[4];
+            int next_last_int = int_sequence.integers[3];
+            Assert.IsTrue(last_int != next_last_int);
         }
     }
 }
--- Mathmagician/Integer.cs
+++ Mathmagician/Integer.cs
@@ -8,15 +8,21 @@ namespace Mathmagician
 {
     public class Integer
     {
-        //private List<int> _integers = new List<int>();
-
+        public int newInteger = -1;
+        public List<int> integers = new List<int>();
         public void GenerateIntegers(int numOfIntegers)
         {
-            var startingNumber = -1;
-            for (int i = 0; i <numOfIntegers; i++ )
+            if (numOfIntegers == 0)
             {
-                startingNumber += 1;
-                Console.WriteLine("> " + startingNumber);
+                Console.WriteLine("Here are the integers you didn't request! I hope you are pleased by their absence. Press any key to close.");
+            }
+            else {
+                for (int i = 0; i < numOfIntegers; i++)
+                {   
+                    newInteger += 1;
+                    Console.WriteLine("> " + newInteger);
+                    integers.Add(newInteger);
+                }
             }
         }
 
