b17f04b41afd8c8b5682e139d8cae01e465f3a24
Ryan B <admirallasers@gmail.com>
2016-08-04 18:06:55 -0500

    added fibonacci unit tests

--- Mathmagician/Fibonacci.cs
+++ Mathmagician/Fibonacci.cs
@@ -8,26 +8,30 @@ namespace Mathmagician
 {
     public class Fibonacci
     {
-        private List<int> _sequence = new List<int> {1, 1};
+        public List<int> sequence = new List<int> {1, 1};
 
         public void GenerateFibonacci(int numOfFibonacci)
         {
-            if (numOfFibonacci < 3 )
+            if (numOfFibonacci == 0 )
+            {
+                Console.WriteLine("Don't want any Fibonacci numbers, eh? More for me, the MATHMAGICIAN!!");
+            }
+            else if ( numOfFibonacci < 3 )
             {
                 for ( int i = 0 ; i < numOfFibonacci; i++ )
                 {
-                    Console.WriteLine("> " + _sequence[i]);
+                    Console.WriteLine("> " + sequence[i]);
                 }
             }
             else
             {
-                Console.WriteLine("> " + _sequence[0]);
-                Console.WriteLine("> " + _sequence[1]);
+                Console.WriteLine("> " + sequence[0]);
+                Console.WriteLine("> " + sequence[1]);
                 for ( int i = 2; i < numOfFibonacci ; i++)
                 {
-                    int newFibonacci = _sequence[i - 2] + _sequence[i - 1];
+                    int newFibonacci = sequence[i - 2] + sequence[i - 1];
                     Console.WriteLine("> " + newFibonacci);
-                    _sequence.Add(newFibonacci);
+                    sequence.Add(newFibonacci);
                 }
             }
             Console.WriteLine("Have I slaked your Fibonacci thirst? The Mathmagician strikes again!");
