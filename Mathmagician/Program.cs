7b719dbbde0b7ae73315517b7c57d78ff18de15b
Ryan B <admirallasers@gmail.com>
2016-08-03 19:27:50 -0500

    initial interface
    program now prompts user for input and responds to keywords

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -10,6 +10,44 @@ namespace Mathmagician
     {
         static void Main(string[] args)
         {
+            string requestInput = "What would you like me to do?";
+            string prompt = "> ";
+            Console.WriteLine("You have summoned the Mathmagician! You must be in need of numbers.");
+            Console.WriteLine("VERY WELL! Input one of my magic words, press the Enter key and watch in awe.");
+            Console.WriteLine("The magic words are: integer, fibonacci, prime, even and odd.");
+            Console.WriteLine(requestInput);
+
+            string userInput = Console.ReadLine().ToLower();
+            if (userInput == "integer")
+            {
+                Console.WriteLine("Integer method requested. press any key to close.");
+                Console.ReadLine();
+            }
+            else if (userInput == "fibonacci")
+            {
+                Console.WriteLine("fibonacci method requested. press any key to close.");
+                Console.ReadLine();
+            }
+            else if (userInput == "prime")
+            {
+                Console.WriteLine("prime method requested. press any key to close.");
+                Console.ReadLine();
+            }
+            else if (userInput == "even")
+            {
+                Console.WriteLine("even method requested. press any key to close.");
+                Console.ReadLine();
+            }
+            else if (userInput == "odd")
+            {
+                Console.WriteLine("odd method requested. press any key to close.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("whoops! incorrect command entered. press any key to close.");
+                Console.ReadLine();
+            }
         }
     }
 }
