319afb51f1ef45a1cdb1d767cf8d6734486aa7b1
Odigene Joseph <Odigene12@gmail.com>
2016-08-04 23:04:13 -0500

    Created class for even number then began unit testing

---
+++ Mathmagician.Tests/EvenNumberTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class EvenNumberTests
+    {
+        [TestMethod]
+        public void MakeSureYouCanMakeEvenNumber()
+        {
+            //Arrange
+
+            //Act
+            EvenNumber usereven = new EvenNumber();
+
+            //Assert
+            Assert.IsNotNull(usereven);
+        }
+
+
+        [TestMethod]
+        public void CheckIfUserNumberIsEven()
+        {
+            EvenNumber neweven = new EvenNumber();
+
+            neweven.usereven = 7;
+
+            Assert.IsFalse(neweven.usereven % 2 == 0);
+
+        }
+
+        [TestMethod]
+        public void CheckIfAdding2Works()
+        {
+            EvenNumber neweven = new EvenNumber();
+
+            neweven.usereven = 4;
+
+            Assert.AreEqual(6, neweven.usereven + neweven.counter);
+        }
+    }
+}
---
+++ Mathmagician/EvenNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class EvenNumber
+    {
+        //create a new number (that's supposed to be even)
+        public int usereven { get; set; }
+
+        //create a counter that will add 2 to each number following the "usereven"
+        public int counter = 2;
+
+        //create a method that adds 2 to the usereven number 
+        public int Add2ToUserEven(int usereven, int counter)
+        {
+            return usereven + counter; 
+        }
+    }
+
+}
