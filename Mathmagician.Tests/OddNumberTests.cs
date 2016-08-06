b22275e9b90305b5db385cbeef98d02722ad8e12
Odigene Joseph <Odigene12@gmail.com>
2016-08-06 09:42:59 -0500

    Finished Odd Numbers and Tests. Even Mumber Branch is the same. Didn't push up correct code, but it should be there now.

---
+++ Mathmagician.Tests/OddNumberTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class OddNumberTests
+    {
+        [TestMethod]
+        public void MakeSureYouCanMakeOddNumber()
+        {
+            //Arrange
+
+            //Act
+            OddNumber userodd = new OddNumber();
+
+            //Assert
+            Assert.IsNotNull(userodd);
+        }
+
+        [TestMethod]
+        public void TestCreateOddListWorks()
+        {
+            OddNumber newodd = new OddNumber();
+
+            List<int> newlist = newodd.CreateOddNumberList(5);
+
+            List<int> testlist = new List<int>() { 1, 3, 5, 7, 9 };
+
+            CollectionAssert.AreEqual(testlist, newlist);
+        }
+
+
+    }
+}
