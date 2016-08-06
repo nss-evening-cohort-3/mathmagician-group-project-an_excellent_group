3ef160e714ae04d33d81e3ab847344fbabdaeec8
Ryan B <admirallasers@gmail.com>
2016-08-06 10:54:43 -0500

    added comments

--- Mathmagician/Fibonacci.cs
+++ Mathmagician/Fibonacci.cs
@@ -8,8 +8,15 @@ namespace Mathmagician
 {
     public class Fibonacci
     {
+        //list to store generated fibonaccis for testing.
+        //contains the first two fibonaccis, ready to go
         public List<int> sequence = new List<int> {1, 1};
 
+        //this method takes an int argument.
+        // if the number of requested fibonaccis is less than 3,
+        //it simply prints off the first or first two numbers in the 'sequence' list.
+        //ask for more than 3, and it will add the last two numbers in the list,
+        //add the generated number to the list, rinse & repeat 
         public void GenerateFibonacci(int numOfFibonacci)
         {
             if (numOfFibonacci == 0 )
