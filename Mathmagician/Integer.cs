a9e10c39c94c893d242efe4eedc1334a2d8ed03d
Ryan B <admirallasers@gmail.com>
2016-08-06 10:47:13 -0500

    added comments

--- Mathmagician/Integer.cs
+++ Mathmagician/Integer.cs
@@ -8,8 +8,14 @@ namespace Mathmagician
 {
     public class Integer
     {
-        public int newInteger = -1;
+        //counter, increments by 1 for each new integer
+        public int newInteger = 0;
+
+        //list to store generated integers for testing
         public List<int> integers = new List<int>();
+        
+        //method takes an int, increments the counter that many times, outputs 
+        //each number along the way & adds it to the list
         public void GenerateIntegers(int numOfIntegers)
         {
             if (numOfIntegers == 0)
@@ -19,13 +25,12 @@ namespace Mathmagician
             else {
                 for (int i = 0; i < numOfIntegers; i++)
                 {   
-                    newInteger += 1;
                     Console.WriteLine("> " + newInteger);
                     integers.Add(newInteger);
+                    newInteger += 1;
                 }
             }
         }
-
     }
 }
   
